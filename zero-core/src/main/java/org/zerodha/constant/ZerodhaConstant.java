e74512af76dca1b89f51d74fdd1fdb3157dd49a3
Punit Tiwari <punittiwari58@gmail.com>
2019-01-06 14:10:40 +0530

    Add zerodha header to util (#18)

---
+++ zero-core/src/main/java/org/zerodha/constant/ZerodhaConstant.java
@@ -0,0 +1,8 @@
+package org.zerodha.constant;
+
+public class ZerodhaConstant {
+	
+	public static final String X_KITE_VERSION = "1.14.0";
+	
+	public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36";
+}
