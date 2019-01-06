e5568a438f278994cacb91101e6adbff063156a8
Punit Tiwari <punittiwari58@gmail.com>
2019-01-06 15:24:01 +0530

    corrected the zerodha brokerage calculation (#19)
    ok

--- zero-core/src/main/java/org/zerodha/domain/Margin.java
+++ zero-core/src/main/java/org/zerodha/domain/Margin.java
@@ -2,46 +2,46 @@ package org.zerodha.domain;
 
 import java.io.Serializable;
 
-import org.zerodha.util.NumberUtil;
 import org.zerodha.util.ZerodhaMarginUtil;
 
 public class Margin implements Serializable {
-	
+
 	private static final long serialVersionUID = 1666185124503742292L;
 
-	private Brokerage broker;
-	
 	private double brokerage;
-	
+
 	private double turnover;
-	
+
 	private double totalBuy;
-	
+
 	private double totalSell;
-	
+
 	private double etc;
-	
+
 	private double gst;
-	
+
 	private double stt;
-	
+
 	private double sebiCharge;
-	
+
 	private double stampDuty;
-	
+
 	private double profit;
-	
+
 	public Margin(Brokerage broker) {
-		this.broker = broker;
 		this.turnover = ZerodhaMarginUtil.fetchTurnOver(broker);
 		this.totalBuy = ZerodhaMarginUtil.fetchTotalBuy(broker);
 		this.totalSell = ZerodhaMarginUtil.fetchTotalSell(broker);
 		this.profit = this.totalSell - this.totalBuy;
+		this.brokerage = ZerodhaMarginUtil.fetchBrokerage(this.turnover);
+		this.etc = ZerodhaMarginUtil.fetchExchangeTnx(broker.getExchange(), turnover);
+		this.gst = ZerodhaMarginUtil.fetchGST(brokerage, etc);
+		this.stt = ZerodhaMarginUtil.fetchSTT(totalSell);
+		this.sebiCharge = ZerodhaMarginUtil.fetchSEBICharge(turnover);
+		this.stampDuty = ZerodhaMarginUtil.fetchStampDuty(turnover);
 	}
 
 	public double getBrokerage() {
-		this.brokerage = NumberUtil.percent(getTurnover(), 0.01);
-		this.brokerage = Math.min(this.brokerage, 20);
 		return brokerage;
 	}
 
@@ -58,35 +58,26 @@ public class Margin implements Serializable {
 	}
 
 	public double getEtc() {
-		if("NSE".equalsIgnoreCase(broker.getExchange())) {
-			this.etc = NumberUtil.percent(getTurnover(), 0.00325);
-		} else if("BSE".equalsIgnoreCase(broker.getBroker())) {
-			this.etc = NumberUtil.percent(getTurnover(), 0.003);
-		}
 		return etc;
 	}
 
 	public double getGst() {
-		this.gst = NumberUtil.percent(getBrokerage()+getGst(), 18);
 		return gst;
 	}
 
 	public double getStt() {
-		this.stt = NumberUtil.percent(getTotalSell(), 0.025);
 		return stt;
 	}
 
 	public double getSebiCharge() {
-		this.sebiCharge = NumberUtil.percent(getTurnover(), 0.0015);
 		return sebiCharge;
 	}
 
 	public double getStampDuty() {
-		this.stampDuty = NumberUtil.percent(getTurnover(), 0.002);
 		return stampDuty;
 	}
-	
+
 	public double getNetPayable() {
-		return profit - (getBrokerage()+getEtc()+getGst()+getSebiCharge()+getStampDuty()+getStt());
+		return profit - (getBrokerage() + getEtc() + getGst() + getSebiCharge() + getStampDuty() + getStt());
 	}
 }
